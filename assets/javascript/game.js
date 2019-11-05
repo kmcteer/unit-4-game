22f2506f80765f9cf450e2a6c14e57534845ebd3
Kaleb M <kay@Kalebs-MacBook-Air.local>
2019-11-04 18:33:31 -0800

    fixed code and moved some code around

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,79 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var stones = []
+    var compPick = Math.floor(Math.random() * 100) + 20;
+    console.log("compPick:" + compPick);
+    $(".randomNumber").text(compPick);
+
+    for (var i = 0; i < 4; i++) {
+        stones.push(Math.floor(Math.random() * 12) + 1);
+
+    }
+    console.log("Stones: " + stones);
+    // //  $("#img1").html("<img src=" + "assets/images/bluestone.jpg" + "value=" + stoneOne + ">");
 
 
-        var comPick = math.floor(math.random() *50) + 12;
-            console.log("compPick:" + compPick);
-            $(".randomNumber").html(compPick);
+    // var stoneTwo = Math.floor(Math.random() * 12) + 1;
+    // console.log("Stone 2: " + stoneTwo);
+    // // $("#img2").html("<img src=" + "assets/images/diamond.jpg" + "value=" + stoneTwo + ">");
+
+    // var stoneThree = Math.floor(Math.random() * 12) + 1;
+    // console.log("Stone 3: " + stoneThree);
+    // //   $("#img3").html("<img src=" + "assets/images/greenstone.jpg" + "value=" + stoneThree + ">");
+
+    // var stoneFour = Math.floor(Math.random() * 12) + 1;
+    // console.log("Stone 4: " + stoneFour);
+    // //   $("#img4").html("<img src=" + "assets/images/redstone.jpg" + "value=" + stoneFour + ">");    
+
+    var wins = 0;
+    console.log("wins: " + wins);
+
+    var losses = 0;
+    console.log("losses:" + losses);
+
+    var score = 0;
+    console.log("score: " + score);
+
+    function reset() {
+        stones = []
+        compPick = Math.floor(Math.random() * 100) + 20;
+        console.log("compPick:" + compPick);
+        $(".randomNumber").text(compPick);
+    
+        for (var i = 0; i < 4; i++) {
+            stones.push(Math.floor(Math.random() * 12) + 1);
+    
+        }
+        console.log("stones new:",stones)
+       score=0  
+       $("#scoreDisplay").text(score);
 
 
-        var stoneOne = Math.floor(Math.random() *75) + 1;
-            console.log("Stone 1: " + stoneOne);
-            $("#img1").html("<img src=" + "assets/images/bluestone.jpg" + "value=" + stoneOne + ">");
+        }
+   
 
+    $(".images").on("click", function () {
+        console.log(this)
+        var index = $(this).attr("data-id")
+        score += stones[index]
+        console.log("score: ", score)
+        //console.log("New Score:" + newScore);
+        $("#scoreDisplay").text(score);
 
-        var stoneTwo = Math.floor(Math.random() *75) + 1;
-            console.log("Stone 2: " + stoneTwo);
-            $("#img2").html("<img src=" + "assets/images/diamond.jpg" + "value=" + stoneTwo + ">");
+        if (score === compPick) {
+            wins++;
+            $("#wins").text(wins);
+            console.log("wins: " + wins);
+            reset();
+        }
 
-        var stoneThree = Math.floor(Math.random() *75) + 1;
-            console.log("Stone 3: " + stoneThree);
-            $("#img3").html("<img src=" + "assets/images/greenstone.jpg" + "value=" + stoneThree + ">");
-
-            var stoneFour = Math.floor(Math.random() *75) + 1;
-            console.log("Stone 4: " + stoneFour);
-            $("#img4").html("<img src=" + "assets/images/redstone.jpg" + "value=" + stoneFour + ">");    
-
-        var wins = 0;
-            console.log("wins: " + wins) ;
-
-        var losses =0;
-            console.log("losses:" + losses) ;
-
-        var score =0;
-            console.log("score: " + score) ;
-
-        function reset () {
-            compPick = Math.floor(Math.random() * 50) + 12;
-                console.log("compPick: " + compPick);
-            $(".randomNumber").html(compPick);
-
-            score= 0;
-            $(".scoreDisplay").html(score);
-
-
-        var stoneOne = Math.floor(Math.random() *75) + 1;
-        console.log("Stone 1: " + stoneOne);
-        $("#img1").html("<img src=" + "assets/images/bluestone.jpg" + "value=" + stoneOne + ">");
-
-
-        stoneTwo = Math.floor(Math.random() *75) + 1;
-        console.log("Stone 2: " + stoneTwo);
-        $("#img2").html("<img src=" + "assets/images/diamond.jpg" + "value=" + stoneTwo + ">");
-
-        stoneThree = Math.floor(Math.random() *75) + 1;
-        console.log("Stone 3: " + stoneThree);
-        $("#img3").html("<img src=" + "assets/images/greenstone.jpg" + "value=" + stoneThree + ">");
-
-        stoneFour = Math.floor(Math.random() *75) + 1;
-        console.log("Stone 4: " + stoneFour);
-        $("#img4").html("<img src=" + "assets/images/redstone.jpg" + "value=" + stoneFour + ">");    
-
-            $("img").on("click", function () {
-                var newScore = score += parseInt($(this).attr("value"));        
-                console.log("New Score: " + newScore);
-                $(scoreDisplay).html(newScore);
-                $(".scoreDisplay").html(newScore);
-
-                if(newScore === compPick) {
-                    wins++ ;
-                    $(".wins").html("wins: " + wins);
-                        console.log("Wins: " + wins);
-                        reset();
-                }
-
-                    else if(newScore > compPick) {
-                        losses++ ;
-                        $(".losses").html("Losses: " + losses);
-                        console.log("Losses: " + losses);
-                        reset();
-
-                    }
-
- });
-}
-
-    $("img").on("click", function () {
-        var newScore = scpre +=parseInt($(this).attr("value"));
-            console.log("New Score:" + newScore);
-            $(".scoreDisplay").html(newScore);
-
-            if(newScore === compPick) {
-                wins++ ;
-                $(".wins").html("Wins: " + wins);
-                    console.log("wins: " + wins);
-                    reset();
-            }
-
-            else if(newScore > compPick) 
-            {
-                lossess++ ;
-                $(".losses").html("losses: " + losses) ;
-                    console.log ("Losses: " + losses);
-                    reset();
-            }
+        else if (score > compPick) {
+            losses++;
+            $("#losses").text(losses);
+            console.log("Losses: " + losses);
+            reset();
+        }
 
     });
+
+})
